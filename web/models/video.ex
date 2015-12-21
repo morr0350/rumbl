bd000b53b0ed00e72cb1807a50810be9361b2069
Tony Morrow <tony.morrow@gmail.com>
2015-12-21 08:24:23 -0600

    permalinks added

--- web/models/video.ex
+++ web/models/video.ex
@@ -1,10 +1,13 @@
 defmodule Rumbl.Video do
   use Rumbl.Web, :model
 
+  @primary_key {:id, Rumbl.Permalink, autogenerate: true}
+
   schema "videos" do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
 
@@ -20,9 +23,32 @@ defmodule Rumbl.Video do
   If no params are provided, an invalid changeset is returned
   with no validation performed.
   """
+
+  defimpl Phoenix.Param, for: Rumbl.Video do
+    def to_param(%{slug: slug, id: id}) do
+        "#{id}-#{slug}"
+    end
+  end
+
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
-    |> foreign_key_constraint(:category_id)
+    |> slugify_title()
+    |> assoc_constraint(:category)
   end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+        put_change(changeset, :slug, slugify(title))
+    else
+        changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/, "-")
+  end
+
 end
